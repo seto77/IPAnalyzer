d079d02bffea6056a8ef191da102cbc75188e36b
seto <seto77@gmail.com>
2023-06-28 14:02:50 +0900

    update

--- Crystallography/Reg.cs
+++ Crystallography/Reg.cs
@@ -3,6 +3,7 @@ using MemoryPack;
 using Microsoft.Win32;
 using System.Threading;
 using System.Text.RegularExpressions;
+using System.Windows.Forms;
 
 namespace Crystallography;
 
@@ -14,22 +15,41 @@ public static class Reg
         if (owner == null)
             return;
         var prop = owner.GetType().GetProperty(propName);
-        var regName = $"{prop.ReflectedType.FullName}.{propName}";
+
+        string regName;
+
+        if(owner is Control c)
+        {
+            if (c.TopLevelControl != null && c.TopLevelControl.Name != c.Name)
+                regName = c.TopLevelControl.Name + "." + c.Name + "." + propName;
+            else
+                regName = c.TopLevelControl.Name + "." + propName;
+        }
+        else if (owner is ToolStripItem t)
+        {
+            regName = t.Name + "." + propName;
+        }
+        else
+            regName = $"{prop.ReflectedType.FullName}.{propName}";
 
         if (mode == Mode.Read)
         {//読込の時
-
-            var buffer = (byte[])key.GetValue(regName);
-            if (buffer == null)
-                return;
-            using var decompressor = new BrotliDecompressor();
-            var val = MemoryPackSerializer.Deserialize<T>(decompressor.Decompress(buffer));
-            if (regName != "System.Globalization.CultureInfo.Name")
-                prop.SetValue(owner, val);
-            else
+            try
             {
-                Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(val.ToString().ToLower().StartsWith("ja") ? "ja" : "en");
+                var buffer = (byte[])key.GetValue(regName);
+
+                if (buffer == null)
+                    return;
+                using var decompressor = new BrotliDecompressor();
+                var val = MemoryPackSerializer.Deserialize<T>(decompressor.Decompress(buffer));
+                if (regName != "System.Globalization.CultureInfo.Name")
+                    prop.SetValue(owner, val);
+                else
+                {
+                    Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(val.ToString().ToLower().StartsWith("ja") ? "ja" : "en");
+                }
             }
+            catch { return; }
         }
 
         else
