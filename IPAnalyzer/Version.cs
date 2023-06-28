08c713427337a40e084e3ceb4945a2ca1fdd2050
seto <seto77@gmail.com>
2023-06-28 14:35:53 +0900

    update

--- IPAnalyzer/Version.cs
+++ IPAnalyzer/Version.cs
@@ -9,6 +9,7 @@ static class Version
 
     static public string History =
         "History" +
+        "\r\n ver3.947(2023/06/28) Improved compatibility for *.img file output from ADXV" +
         "\r\n ver3.946(2023/04/22) Changed profile format to '*.pdi2'." +
         "\r\n ver3.945(2023/03/18) Fixed a minor bug." +
         "\r\n ver3.944(2023/03/07) Fixed a minor bug." +
